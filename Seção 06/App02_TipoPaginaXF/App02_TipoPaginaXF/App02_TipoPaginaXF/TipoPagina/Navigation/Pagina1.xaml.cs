b40ecf2f048ac08a7628bf19676161d0e518e41c
Eliezer Moura <eliezer.newmemx003@gmail.com>
2019-08-20 11:18:55 -0300

    Projeto Tipos de Pagina

---
+++ Seção 06/App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Navigation/Pagina1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPaginaXF.TipoPagina.Navigation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Pagina1 : ContentPage
+	{
+		public Pagina1 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void MudarPagina2(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Pagina2());
+        }
+
+        
+        private void ChamarModal(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new Modal());
+        }
+
+        private void ChamarMaster(object sender, EventArgs args)
+        {
+            App.Current.MainPage = new Master.Master();
+        }
+    }
+}
