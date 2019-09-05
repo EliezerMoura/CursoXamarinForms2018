cd05d02ba6bef1d790c422c524d6bf7039c086c8
Eliezer Moura <eliezer.newmemx003@gmail.com>
2019-09-05 14:17:47 -0300

    App 3 - Layouts

---
+++ Seção 07/App01_LayoutXF/App01_LayoutXF/App01_LayoutXF/Layouts/Stack/StackPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_LayoutXF.Layouts.Stack
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StackPage : ContentPage
+	{
+		public StackPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
